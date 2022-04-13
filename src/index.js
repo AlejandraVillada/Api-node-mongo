1ab9d1d21c8e63370f5acfdc9b5ce53af6056b47
AlejandraVillada <villadatorresmariaalejandra@gmail.com>
2022-04-13 00:04:36 -0500

    api_completa

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const app = require('./app')
+
+
+
+app.listen(app.get('port'), () => {
+    console.log('Se está corriendo en el puerto 3000')
+    console.log('nombre de la aplicación', app.get("name"));
+})
